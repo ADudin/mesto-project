18abbb0af34733f57cf31df7c88aa3ac7fa13239
ADudin <gwaeron27@yandex.ru>
2023-01-08 14:36:40 +0300

    добавил отрисовку шести карточек "из коробки"

--- script.js
+++ script.js
@@ -134,6 +134,39 @@ handleNewCardForm = () => {
   newCardForm.addEventListener('submit', handleNewCardFormSubmit);
 }
 
+// initial cards rendering
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach((item) => {
+  addNewCard(item.name, item.link);
+});
+
 // main eventListeners
 
 profileEditButton.addEventListener('click', () => {
